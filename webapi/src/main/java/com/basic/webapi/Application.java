6b6cf91200f9c460ecdf08fb02d9fee96e25eec2
yuqijun <yuqijun@nihaosi.com>
2021-07-29 17:05:40 +0800

    封装基础依赖第一版持续集成 -> 添加spring-test 依赖

--- webapi/src/main/java/com/basic/webapi/Application.java
+++ webapi/src/main/java/com/basic/webapi/Application.java
@@ -2,7 +2,6 @@ package com.basic.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication()
 public class Application {
     public static void main(String [] args){
